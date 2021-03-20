bb4e16769aed5bbfd81b85aa9f528b847a60e54b
shinyul <ehszl409@naver.com>
2021-03-20 15:52:53 +0900

    2. 검색 결과 UI v1 03.20

---
+++ app/src/main/java/com/cos/floui/SearchResultActivity.java
@@ -0,0 +1,14 @@
+package com.cos.floui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+    }
+}
